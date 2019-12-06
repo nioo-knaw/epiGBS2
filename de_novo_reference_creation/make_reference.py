00474516beb8b34458cf2b886d320711d4645e3c
Maarten Postuma <maarten.postuma@wur.nl>
2019-12-06 15:09:38 +0100

    Updated python version

--- de_novo_reference_creation/make_reference.py
+++ de_novo_reference_creation/make_reference.py
@@ -33,7 +33,7 @@ usearch = "usearch"#_8.0.1409_i86osx32"
 vsearch = "vsearch"
 seqtk = "seqtk"
 pear = "pear"
-create_consensus = "de_novo_reference_creation/create_consensus.py"
+#create_consensus = "de_novo_reference_creation/create_consensus.py"
 vcfutils = "vcfutils.pl"
 
 def parse_args():
@@ -518,7 +518,7 @@ def cluster_consensus(in_files,args):
     # in_files['consensus']['consensus_clustered'] = args.consensus_cluster
     log = "rename cluster_cons for bwa_meth compatibility"
     cluster_renamed = args.consensus_cluster.replace('.fa','.renamed.fa')
-    cmd = ['cat %s | python2 de_novo_reference_creation/rename_fast.py -n > %s'%(args.consensus_cluster, cluster_renamed)]
+    cmd = ['cat %s | python de_novo_reference_creation/rename_fast.py -n > %s'%(args.consensus_cluster, cluster_renamed)]
     run_subprocess(cmd,args,log)
     log = "faidx index %s" % cluster_renamed
     cmd = ["samtools faidx %s" % cluster_renamed]
