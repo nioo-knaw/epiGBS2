c9e76eff6c62193e01589ac4542a119308b4aa17
Postuma <maarten.postuma@wur.nl>
2021-04-20 11:47:41 +0200

    Update paramTestPlot.R

--- src/parameter_test/paramTestPlot.R
+++ src/parameter_test/paramTestPlot.R
@@ -2,22 +2,22 @@ rm(list=ls())
 library(ggplot2)
 library(reshape2)
 args<-commandArgs()
-data<-read.table(args[1])
+dataDenovo<-read.table(args[1])
 
 colorBlindBlack8  <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
                        "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-data$V7<-as.numeric(sub("%","",data$V7))
-data$V6<-data$V6/data$V5*100
-data$V9<-data$V3-data$V4
-data<-data[-c(5)]
-colnames(data)<-c("minDepth","clusterID","number of clusters","number of unassembled cluster","percentage multimapped","mapping percentage","average depth","number of assembled clusters")
+dataDenovo$V7<-as.numeric(sub("%","",dataDenovo$V7))
+dataDenovo$V6<-dataDenovo$V6/dataDenovo$V5*100
+dataDenovo$V9<-dataDenovo$V3-dataDenovo$V4
+dataDenovo<-dataDenovo[-c(5)]
+colnames(dataDenovo)<-c("minDepth","clusterID","number of clusters","number of unassembled cluster","percentage multimapped","mapping percentage","average depth","number of assembled clusters")
 
 
-meltedData<-melt(data,id.vars = c("minDepth","clusterID"))
+melteddataDenovo<-melt(dataDenovo,id.vars = c("minDepth","clusterID"))
 
 
-ggplot(meltedData,aes(x=minDepth,y=value,col=as.factor(clusterID)))+geom_line(size=0.5)+facet_wrap(.~variable,scales = "free")+theme_light()+xlab("minimal depth")+geom_point(size=2)+
+ggplot(melteddataDenovo,aes(x=minDepth,y=value,col=as.factor(clusterID)))+geom_line(size=0.5)+facet_wrap(.~variable,scales = "free")+theme_light()+xlab("minimal depth")+geom_point(size=2)+
   scale_colour_manual("Clustering ID %",values = colorBlindBlack8)+
   ggsave(args[2],height=9,width=9,dpi="retina")
 
