28ee87414699668545ed6f8d9437ba070087857b
Postuma <smart-post@hotmail.com>
2024-03-21 11:49:53 +0100

    Barcode creation should work now. It did not split per  run...

--- src/demultiplex/createFilesFromBarcodeFile.R
+++ src/demultiplex/createFilesFromBarcodeFile.R
@@ -13,9 +13,9 @@ write.table(barcodeStacks,paste0(args[2],"/barcode_stacks.tsv"),row.names = F,co
 
 for(run in barcodes$run){
   barcodesSub<-barcodes[barcodes$run==run,]
-barcodeStacks<-data.frame(barcode1=c(paste0(barcodes$Barcode_R1,"T"),paste0(barcodes$Barcode_R1,"C")),
-                          barcode2=c(paste0(barcodes$Barcode_R2,"C"),paste0(barcodes$Barcode_R2,"T")),
-                          sample=c(paste0(barcodes$Sample,"-Watson"),paste0(barcodes$Sample,"-Crick")))
+barcodeStacks<-data.frame(barcode1=c(paste0(barcodesSub$Barcode_R1,"T"),paste0(barcodesSub$Barcode_R1,"C")),
+                          barcode2=c(paste0(barcodesSub$Barcode_R2,"C"),paste0(barcodesSub$Barcode_R2,"T")),
+                          sample=c(paste0(barcodesSub$Sample,"-Watson"),paste0(barcodesSub$Sample,"-Crick")))
   write.table(barcodeStacks,paste0(args[2],"/barcode_stacks",run,".tsv"),row.names = F,col.names = F,quote=F,sep="\t")
               
               
