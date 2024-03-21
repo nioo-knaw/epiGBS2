ca73cb9b80cc8dbcf2a6d2fcebf17ff2c63ba941
Postuma <smart-post@hotmail.com>
2024-03-21 11:22:35 +0100

    Capitalisation in the barcode file is important

--- src/demultiplex/createFilesFromBarcodeFile.R
+++ src/demultiplex/createFilesFromBarcodeFile.R
@@ -6,16 +6,16 @@ args = commandArgs(trailingOnly=TRUE)
 barcodes<-read.table(args[1],h=T,sep="\t")
 barcodes$run<-sub("_R1.fq.gz","",barcodes$rawR1)
 
-barcodeStacks<-data.frame(sample=c(paste0(barcodes$sample,"-Watson"),paste0(barcodes$sample,"-Crick")),
-                          barcode1=c(paste0(barcodes$barcode1,"T"),paste0(barcodes$barcode1,"C")),
-                          barcode2=c(paste0(barcodes$barcode2,"C"),paste0(barcodes$barcode2,"T")))
+barcodeStacks<-data.frame(sample=c(paste0(barcodes$Sample,"-Watson"),paste0(barcodes$Sample,"-Crick")),
+                          barcode1=c(paste0(barcodes$Barcode1,"T"),paste0(barcodes$Barcode1,"C")),
+                          barcode2=c(paste0(barcodes$Barcode2,"C"),paste0(barcodes$Barcode2,"T")))
 write.table(barcodeStacks,paste0(args[2],"/barcode_stacks.tsv"),row.names = F,col.names = F,quote=F,sep="\t")
 
 for(run in barcodes$run){
   barcodesSub<-barcodes[barcodes$run==run,]
-barcodeStacks<-data.frame(sample=c(paste0(barcodes$sample,"-Watson"),paste0(barcodes$sample,"-Crick")),
-                          barcode1=c(paste0(barcodes$barcode1,"T"),paste0(barcodes$barcode1,"C")),
-                          barcode2=c(paste0(barcodes$barcode2,"C"),paste0(barcodes$barcode2,"T")))
+barcodeStacks<-data.frame(sample=c(paste0(barcodes$Sample,"-Watson"),paste0(barcodes$Sample,"-Crick")),
+                          barcode1=c(paste0(barcodes$Barcode1,"T"),paste0(barcodes$Barcode1,"C")),
+                          barcode2=c(paste0(barcodes$Barcode2,"C"),paste0(barcodes$Barcode2,"T")))
   write.table(barcodeStacks,paste0(args[2],"/barcode_stacks",run,".tsv"),row.names = F,col.names = F,quote=F,sep="\t")
               
               
